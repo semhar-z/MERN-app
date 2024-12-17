f68dcc54dca9d5acde0996bfa3b50173f8666524
semhar-z <semharzewoldi@gmail.com>
2024-12-17 12:16:02 -0500

    port change

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.use("/api/items", itemRoutes);
 
 // Connect to MongoDB
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(port, () => console.log(`Server running on port ${port}`));
   })
   .catch(err => console.error(err));
 
