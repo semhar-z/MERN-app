87f410a75069e0642f287a68f7765f43c9ccc0eb
semhar-z <semharzewoldi@gmail.com>
2024-12-19 00:33:33 -0500

    another change

--- routes/items.js
+++ routes/items.js
@@ -38,14 +38,14 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.get('/:postId', async (req, res) => {
+router.get("/:postId", async (req, res) => {
   try {
     const { postId } = req.params;
       if (isNaN(postId)) {
       return res.status(400).json({ error: "Invalid number parameter" });
     }
     
-    const item = await Item.findOne({ number: parseInt(postId) });
+    const item = await Item.findOne({ postId: parseInt(postId) });
     if (!item) {
       return res.status(404).json({ error: "Item not found" });
     }
